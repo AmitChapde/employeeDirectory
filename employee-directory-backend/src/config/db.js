75dc35c00bc1c0491afc290babf729154a41f7b6
Amit Chapde <amitchapde2@gmail.com>
2025-11-29 13:08:28 +0530

    feat:added db URI changes

--- employee-directory-backend/src/config/db.js
+++ employee-directory-backend/src/config/db.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectDB = async () => {
-  const url = `mongodb+srv://jack:w8tItiTf0FsFCFTp@cluster0.jywt36i.mongodb.net/?appName=Cluster0`;
   try {
-    await mongoose.connect(url);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB Error:", err.message);
